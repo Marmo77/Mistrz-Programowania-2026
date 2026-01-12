912024d00e0a85af3371725c0f9dd35027940f2e
Marmo77 <matyskamela@gmail.com>
2026-01-12 14:41:18 +0100

    runda 2 - kurtka 100/100

--- Runda1/Plan_konwoj/main.py
+++ Runda1/Plan_konwoj/main.py
@@ -2,45 +2,59 @@ from collections import defaultdict, deque
 
 
 class plan_konwoj:
-    def plan_konwoj(self, n: int, m: int) -> int:
-        polaczenia: list[tuple[int, int, int]] = []
-        for _ in range(m):
-            odczytaj = input()
-            a,b,c = map(int, odczytaj.split(' ')) # dzieli input na trzy czesci -> 1 2 3 i przypisuje do zmiennych
-            # a -> b - połączenie miedzy muzeami o danych id, c -> poziom zagrożenia
-            polaczenia.append((a,b,c))
+    def __init__(self, polaczenia: list[tuple[int, int, int]]):
+        self.polaczenia = polaczenia
 
-        print("-----------")
-        # graf: dict[int, list[tuple[int, int]]] = {}
-        graf: dict[int, list[tuple[int, int]]] = defaultdict(list)
-
-        for a,b,c in polaczenia:
-            # if a not in graf:
-            #     graf[a] = []
-            # if b not in graf:
-            #     graf[b] = []
+    def uzupelnianie_grafu(self):
+        graf: dict[int, list[tuple(int,int)]] = defaultdict(list) # -> wartosc domyslna [] dla list
+        # tworzenie grafu - dla kazdego podanego muzeum a dodajemy (b, c) do grafu - b -> muzeum z polaczeniem, c -> poziom zagrozenia
+        for a,b,c in self.polaczenia: # dla kazdego polaczenia utworz graf i dodaj tam jego wartosci
             graf[a].append((b,c))
             graf[b].append((a,c)) # dwukierunkowa, czyli w obie strony
-        # print(graf.items())
-        print(graf)
+        # print(graf)
+        return graf
+        # przykładowy wygląd grafu:
+        # {
+        #     1: [(2, 1), (5, 8)], 
+        #     2: [(1, 1), (3, 3), (4, 2), (5, 1)], 
+        #     3: [(2, 3), (4, 2), (5, 0)], 
+        #     4: [(2, 2), (3, 2)], 
+        #     5: [(1, 8), (2, 1), (3, 0)]
+        # }
 
-        
-        # print(polaczenia)
-        trasy: list[tuple[int,int]] = []
-        q = int(input()) # q -> liczba przejazdów
-        for _ in range(q):
+    def trasy_konwojow(self, q:int): # zwraca nam liste przejazdów konwojów
+        trasy: list[tuple[int, int]] = []
+        for _ in range(int(q)):
             odczytaj = input()
             x,y = map(int, odczytaj.split(' '))
             trasy.append((x,y))
             # x -> y - przejazdź z muzea o id x do muzea o id y
-        
-        print("------------")
+        return trasy
 
-        for x,y in trasy:
-            for z in range(0, n+1):
-                if self.da_sie_dojsc(graf, x, y, z):
-                    print(z)
-                    break
+
+    def plan_konwoj(self, n: int, m: int) -> int:
+        graf = self.uzupelnianie_grafu()
+        # po uzupelnieniu grafu, pytamy o ilosc przejazdow i ich polaczenia
+        q = input() # q -> liczba przejazdów
+        trasy = self.trasy_konwojow(q)
+
+        # print(graf)
+        print("------------")
+        # print(trasy)
+
+        # teraz musimy sprawdzic czy da sie przejechac od muzeum x do muzeum y,
+        # najproszym konwojem, z najmniejszym możliwmy zagrożeniem
+        # ---- np. 
+        # x = 1, y = 5
+        # istnieje takie polaczenie 1->5, ale ma zagrozenie 8, sprawdzamy czy da sie dojsc do 5 z mniejszym zagrozeniem
+        # 1->2->5 ma zagrozenie 1, 1, wiec da sie dojsc do 5 z zagrozeniem 1 
+        for x, y in trasy: # x - muzeum startowe, y - muzeum docelowe
+            for sasiad, zagrozenie in graf[x]: # sasiad - muzeum obok, zagrozenie - poziom zagrozenia na trasie do sasiada
+                if sasiad == y: # jesli sasiad jest rowny y to znaczy ze da sie dojsc do y z zagrozeniem o poziomie 'zagrozenie'
+                    print(f"Muzeum {x}, sasiad {sasiad}, zagrozenie {zagrozenie}") # wypisuje nam zagrozenie dla kazdej trasy naszego sasiada
+
+
+
 
     def da_sie_dojsc(self, graf, start, cel, max_zagrozenie):
         odwiedzone = set()
@@ -61,16 +75,17 @@ class plan_konwoj:
         return False
 
 
-# ZADANIE DO ANALIZY I DOWIEDZIEC SIE CO ROBI defaultdict I DEQUE 
-        
-
-            
-
-
-
 
 if __name__ == "__main__":
-    n,m = map(int, input().split()) # n - liczba muzeow, m - liczba konwojow
 
-    zadanie = plan_konwoj()
-    print(zadanie.plan_konwoj(n,m))
+    polaczenia: list[tuple[int, int, int]] = []
+
+    # wczytywanie polaczen z inputa
+    n,m = map(int, input().split()) # n - liczba muzeow, m - liczba konwojow
+    for _ in range(m):
+        a,b,c = map(int, input().split(' ')) # dzieli input na trzy czesci -> 1 2 3 i przypisuje do zmiennych
+        # a -> b - połączenie miedzy muzeami o danych id, c -> poziom zagrożenia
+        polaczenia.append((a,b,c))
+
+    zadanie = plan_konwoj(polaczenia)
+    zadanie.plan_konwoj(n, m)
---
+++ Runda2/Kurtka/main.py
@@ -0,0 +1,24 @@
+class Kurtka:
+    pass
+    def czy_kurtka_jest_bezpieczna(self, n, a, b, c) -> str: # n jest nam nie potrzebne
+
+        dotyka = False
+
+        if c >= a and c <= b: # jesli c znajduje sie w przedziale a-b np. (a=6, b=10, c=8 - jest w przedziale)
+            dotyka = True
+
+        if dotyka:
+            return "Nie dotykaj mojej kurtki!"
+        else:
+            return "Spokojnie."
+        
+
+
+if __name__ == "__main__":
+
+    powieszona_kurtka = input()
+    
+    n,a,b,c = map(int, powieszona_kurtka.split()) # n -> ilosc haczykow, a - b -> karol zajmuje haczyki od a do b, c -> agent kombinuje przy tym haczyku
+
+    kurtka = Kurtka()
+    print(kurtka.czy_kurtka_jest_bezpieczna(n, a, b, c))
